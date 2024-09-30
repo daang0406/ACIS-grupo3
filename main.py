4df10195d2aadd4e04e5a1470a05d15269593753
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-09-30 09:49:45 -0500

    Update main.py
    cambio de ruta de la imagen a raw

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def main():
         st.write('Esta es una aplicación de la cual se encarga de transformar imágenes de CT a US y viceversa')
         
         # Cargar la imagen
-        image_path = 'https://sl.bing.net/hDEw62keLng'  # Ruta de tu imagen
+        image_path = 'https://raw.githubusercontent.com/daang04/ACIS-grupo3/main/ha.png' # Ruta de tu imagen
         st.image(image_path, width=300)
         
         # Sección: ¿Cuáles son los parámetros empleados?
